c649ae96073b5916b0df373fcf29fe1cac0db68b
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2025-02-02 18:49:00 -0400

    :heart: Added quantum time 5 Round robin

--- src/main/java/Scheduler/RoundRobin.java
+++ src/main/java/Scheduler/RoundRobin.java
@@ -4,10 +4,49 @@
  */
 package Scheduler;
 
+import EDD.Queue;
+import Process.Process;
+
 /**
  *
  * @author Andrea
  */
 public class RoundRobin {
-    
+      private int quantum= 5;  // Hay q agregar algo en la interfaz que permita preguntarle al usuario esto puede ser 5,10,15
+
+    // Constructor for the quantum
+    public RoundRobin(int quantum) {
+        this.quantum = quantum;
+    }
+
+    public void executeRoundRobin(Queue<Process> queue) {
+        Queue<Process> readyQueue = new Queue<>();  //Ready queue
+        int currentTime = 0;
+
+        // Adds proccess to queue
+        while (!queue.isEmpty()) {
+            readyQueue.enqueue(queue.dequeue());
+        }
+
+        // runs processes following roundrobin
+        while (!readyQueue.isEmpty()) {
+            Process currentProcess = readyQueue.dequeue();
+            int remainingBurstTime = currentProcess.getInstructionCount();
+
+            System.out.println("Time: " + currentTime + " - Running process: " + currentProcess.getprocessName());
+
+            if (remainingBurstTime > quantum) {
+                // If time > quantum
+                currentProcess.setInstructionCount(remainingBurstTime - quantum);  // minus left time
+                currentTime += quantum;  // Adds to the global time of the quantum
+                System.out.println("Time: " + currentTime + " - Running process " + currentProcess.getprocessName() + " paused, time left: " + currentProcess.getInstructionCount());
+                readyQueue.enqueue(currentProcess);  // Reinsterts the process in the queue
+            } else {
+                // process time < quantum
+                currentTime += remainingBurstTime;  // Adds to the global time
+                System.out.println("Time: " + currentTime + " - Process " + currentProcess.getprocessName() + " completed");
+            }
+        }
+        System.out.println("All processes have been executed.");
+    }
 }
